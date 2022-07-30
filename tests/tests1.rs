7e268c90516c0e66fac162457262e60088a3851f
Adasumizox <darkdan099@gmail.com>
2022-07-30 12:52:40 +0200

    Rustlings solutions

---
+++ tests/tests1.rs
@@ -0,0 +1,21 @@
+// tests1.rs
+// Tests are important to ensure that your code does what you think it should do.
+// Tests can be run on this file with the following command:
+// rustlings run tests1
+
+// This test has a problem with it -- make the test compile! Make the test
+// pass! Make the test fail!
+// Execute `rustlings hint tests1` or use the `hint` watch subcommand for a hint.
+
+pub fn compare(a: i32, b: i32) -> bool{
+    a == b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn you_can_assert() {
+        assert!(compare(1, 1), true);
+    }
+}
---
+++ tests/tests2.rs
@@ -0,0 +1,17 @@
+// tests2.rs
+// This test has a problem with it -- make the test compile! Make the test
+// pass! Make the test fail!
+// Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
+
+pub fn compare(a: i32, b: i32) -> bool {
+    a == b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn you_can_assert_eq() {
+        assert_eq!(compare(10, 8), false);
+    }
+}
